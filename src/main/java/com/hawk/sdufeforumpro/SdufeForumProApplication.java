a361ba790ba6a2d4bdc9b29d8e69d5c757ce0711
HAWK97 <wsghawk97@163.com>
2024-09-04 15:50:20 +0800

    创建项目

---
+++ src/main/java/com/hawk/sdufeforumpro/SdufeForumProApplication.java
@@ -0,0 +1,13 @@
+package com.hawk.sdufeforumpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdufeForumProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdufeForumProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hawk/sdufeforumpro/SdufeForumProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hawk.sdufeforumpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdufeForumProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
