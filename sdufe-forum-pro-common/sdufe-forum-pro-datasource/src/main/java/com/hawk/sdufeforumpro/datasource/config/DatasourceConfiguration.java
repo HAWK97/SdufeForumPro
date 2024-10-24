a01bc193b2ae3d3ab62c52f76a529008200f30ae
HAWK97 <wsghawk97@163.com>
2024-10-24 15:01:02 +0800

    add sdufe-forum-pro-datasource模块

---
+++ sdufe-forum-pro-common/sdufe-forum-pro-datasource/src/main/java/com/hawk/sdufeforumpro/datasource/config/DatasourceConfiguration.java
@@ -0,0 +1,35 @@
+package com.hawk.sdufeforumpro.datasource.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.hawk.sdufeforumpro.datasource.handler.DataObjectHandler;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = "com.hawk.sdufeforumpro.*.infrastructure.mapper")
+public class DatasourceConfiguration {
+
+    @Bean
+    public DataObjectHandler getDataObjectHandler() {
+        return new DataObjectHandler();
+    }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        //防全表更新与删除插件
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+        //分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
