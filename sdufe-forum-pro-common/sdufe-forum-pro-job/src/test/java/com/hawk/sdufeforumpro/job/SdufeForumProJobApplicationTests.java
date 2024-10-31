a7a210389a9e878e825f79538da5a602887ce2aa
HAWK97 <wsghawk97@163.com>
2024-10-31 19:55:31 +0800

    add sdufe-forum-pro-job模块

---
+++ sdufe-forum-pro-common/sdufe-forum-pro-job/src/test/java/com/hawk/sdufeforumpro/job/SdufeForumProJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hawk.sdufeforumpro.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdufeForumProJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
