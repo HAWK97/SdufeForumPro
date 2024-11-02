0f0a87e15d26edb246633e1c3ddbf0ea5140077f
HAWK97 <wsghawk97@163.com>
2024-11-02 10:01:23 +0800

    add sdufe-forum-pro-gateway模块

---
+++ sdufe-forum-pro-gateway/src/main/java/com/hawk/sdufeforumpro/gateway/SdufeForumProGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hawk.sdufeforumpro.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdufeForumProGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdufeForumProGatewayApplication.class, args);
+    }
+
+}
---
+++ sdufe-forum-pro-gateway/src/test/java/com/hawk/sdufeforumpro/gateway/SdufeForumGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hawk.sdufeforumpro.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdufeForumGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
